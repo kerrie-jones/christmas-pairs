eed7a4d34adc0452e159203e39b70cfa4342f671
kerrie-jones <kerriejones81@icloud.com>
2022-12-03 18:16:40 +0000

    Add flip counter for each flip

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,24 +82,30 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     ]
 
-    //  Create board, flip card and check for match code taken from https://www.youtube.com/watch?v=tjyDOHzKN0w Make Memory Game by Ania Kubow
+    //  Randomize, Create board, flip card and check for match code taken from https://www.youtube.com/watch?v=tjyDOHzKN0w Make Memory Game by Ania Kubow
 // Randomise cards for each new board
     cardArray.sort(() => 0.5 - Math.random())
 
     const grid = document.querySelector('.grid')
     const resultDisplay = document.querySelector('#score')
+    const flipDisplay = document.querySelector('#flips')
     var cardsChosen = []
     var cardsChosenId = []
     var cardsWon = []
+    var flips = []
+    
+
+
 
     // Create game board
     function createBoard() {
+        // loops over all the cards in the array and then appends them to the grid
         for (let i = 0; i < cardArray.length; i++) {
             var card = document.createElement('img')
             card.setAttribute('src', '../assets/images/snowflake.jpg')
             card.setAttribute('data-id', i)
             card.addEventListener('click', flipCard)
-            // Add images to .grid
+            // Adds images to .grid
             grid.appendChild(card)
         }
     }
@@ -107,33 +113,36 @@ document.addEventListener('DOMContentLoaded', () => {
     // Check for match
     function checkForMatch() {
         var cards = document.querySelectorAll('img')
-        const optionOneId = cardsChosenId[0]
-        const optionTwoId = cardsChosenId[1] 
+        const firstCardId = cardsChosenId[0]
+        const secondCardId = cardsChosenId[1] 
         if (cardsChosen[0] === cardsChosen[1]) {
-            alert ('You found a pair!')
-            cards[optionOneId].setAttribute('src', '../assets/images/green_card.jpg')
-            cards[optionTwoId].setAttribute('src', '../assets/images/green_card.jpg')
+            alert ('You found a match!')
+            cards[firstCardId].setAttribute('src', '../assets/images/green_card.jpg')
+            cards[secondCardId].setAttribute('src', '../assets/images/green_card.jpg')
             cardsWon.push(cardsChosen)
         } else {
             // if dont match flip back over and display snowflake 
-            cards[optionOneId].setAttribute('src', '../assets/images/snowflake.jpg')
-            cards[optionTwoId].setAttribute('src', '../assets/images/snowflake.jpg')
-            alert('Sorry! Try again!')
+            cards[firstCardId].setAttribute('src', '../assets/images/snowflake.jpg')
+            cards[secondCardId].setAttribute('src', '../assets/images/snowflake.jpg')
+            alert('Sorry! Try again, not a match!')
         }
         // if either happens clear cardschosen array and cardschosenId ready to start flipping again
         cardsChosen = []
         cardsChosenId = []
-// If no of cards won = length of full array /2 all matches have been found
+
+// If number of cards won = length of full array /2 all matches have been found
         resultDisplay.textContent = cardsWon.length
         if (cardsWon.length === cardArray.length/2) {
             resultDisplay.textContent = 'Congratulations! You found all the pairs'
         }
+        // flipDisplay.textContent = 
         
     }
 
 
     // Flip Card
     function flipCard() {
+        flips++;
         var cardId = this.getAttribute('data-id')
         cardsChosen.push(cardArray[cardId].name)
         cardsChosenId.push(cardId)
@@ -141,6 +150,8 @@ document.addEventListener('DOMContentLoaded', () => {
         if (cardsChosen.length === 2) {
             setTimeout(checkForMatch, 500)
         }
+flipDisplay.textContent = flips;
+
     }
     
 
